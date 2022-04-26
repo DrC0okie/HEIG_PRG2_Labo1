39b8a7e713003121a56cf4c9b0fd2bb253249a4f
DrCookie <timothee.vanhove@heig-vd.ch>
2022-04-26 20:02:45 +0200

    Added user input % modified .gitignore

--- main.c
+++ main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-   printf("Hello, World!\n");
+const unsigned MIN = 11000, MAX = 30000;
+#define MSG_UTILISATEUR "Entrez le nombre de billes [%u - %u] :",MIN, MAX
+
+void viderBuffer(void);
+
+int main(void) {
+   unsigned nbBilles;
+   printf(MSG_UTILISATEUR);
+   while (scanf("%u", &nbBilles) != 1 || nbBilles < MIN || nbBilles > MAX) {
+      printf("%s\n", "Saisie incorrecte. Veuillez SVP recommencer.");
+      viderBuffer();
+      printf(MSG_UTILISATEUR);
+   }
+   viderBuffer();
+
+
    return 0;
+}
+
+void viderBuffer(void) {
+   int c;
+   do {
+      c = getchar();
+   } while (c != '\n' && c != EOF);
 }
