ef51f72893d0917199e782163af41d34ec8637d8
Kumo-chan <71428805+Kumo-chan@users.noreply.github.com>
2022-04-28 12:41:03 +0200

    Simulation de bille au lieu de clous

--- main.c
+++ main.c
@@ -17,95 +17,98 @@ Compilateur    : Mingw-w64 gcc 11.2.0
 #include <stdlib.h>
 #include <string.h>
 
-#define NB_CLOUS(ETAGE) ((ETAGE)*((ETAGE)+1)/2)
-#define ENFANT_GAUCHE(ETAGE, PARENT) ((PARENT) + (ETAGE))
-#define ENFANT_DROITE(ETAGE, PARENT) ((PARENT) + (ETAGE) + 1)
-
 const unsigned MIN_BILLE = 1000, MAX_BILLE = 30000, MIN_RANGEE = 10, MAX_RANGEE = 20;
 const char *MSG_ERREUR = "Saisie incorrecte. Veuillez SVP recommencer.";
 const char *MSG_BILLES = "Entrez le nombre de billes";
 const char *MSG_RANGEES = "Entrez le nombre de rangees de compteurs";
 
 void viderBuffer(void);
-
+unsigned nbClous(unsigned nbEtage);
 unsigned entreeUtilisateur(const char *msg, const char *msgErreur, unsigned min,
                            unsigned max);
-void imprimerCompteurs(unsigned* tableau, unsigned nbRangees);
-void imprimerLigneCompteurs(unsigned* tableau, unsigned numLigne);
+void parcoursBille(unsigned* tab, unsigned nbEtage);
+void imprimerCompteurs(unsigned *tableau, unsigned nbRangees);
 
-unsigned separerGauche(unsigned valeur);
+void imprimerLigneCompteurs(unsigned *tableau, unsigned numLigne);
+
 
 int main(void) {
-    unsigned nbBilles =entreeUtilisateur(MSG_BILLES, MSG_ERREUR, MIN_BILLE, MAX_BILLE);
-    unsigned nbEtage = entreeUtilisateur(MSG_RANGEES, MSG_ERREUR, MIN_RANGEE, MAX_RANGEE);
-   nbEtage = 10;
-    nbBilles = 30000;
-   unsigned* tabCompteur = calloc(NB_CLOUS(nbEtage) , sizeof(unsigned));
-    tabCompteur[0] = nbBilles;
-    int i = 0;
-    // Parcours du tableau
-    for( unsigned etage = 0, index = 0; etage < nbEtage; ++etage) {
-        for (unsigned colonne =0; colonne < etage; ++colonne, ++index) {
-            unsigned valeurGauche = separerGauche(tabCompteur[index]);
-            tabCompteur[ENFANT_GAUCHE(etage, index)] += valeurGauche;
-            tabCompteur[ENFANT_DROITE(etage, index)] += tabCompteur[index] - valeurGauche;
-        }
+    unsigned nbBilles = entreeUtilisateur(MSG_BILLES, MSG_ERREUR, MIN_BILLE, MAX_BILLE);
+    unsigned nbEtage =  entreeUtilisateur(MSG_RANGEES, MSG_ERREUR, MIN_RANGEE, MAX_RANGEE);
 
-        printf("\n");
+    unsigned *tabCompteur = calloc(nbClous(nbEtage), sizeof(unsigned));
+
+
+    for(unsigned bille = 0; bille < nbBilles; ++bille) {
+        parcoursBille(tabCompteur, nbEtage);
     }
-    for( unsigned etage = 0, index = 0; etage <= nbEtage; ++etage) {
-        for (unsigned colonne =0; colonne < etage; ++colonne, ++index) {
+    //affichage tableau (pas beau)
+    for (unsigned etage = 0, index = 0; etage <= nbEtage; ++etage) {
+        for (unsigned colonne = 0; colonne < etage; ++colonne, ++index) {
             printf("valeur %d ", tabCompteur[index]);
         }
         printf("\n");
     }
-    //nettoyage de la mémoire
-    free(tabCompteur);
 
-   unsigned tableauTest[] = {123,120,3,80,20,23,40,40,40,3};
-   imprimerCompteurs(tableauTest, 4);
-   return EXIT_SUCCESS;
+    free(tabCompteur);
+    return EXIT_SUCCESS;
 }
 
 void viderBuffer(void) {
-   int c;
-   while ((c = getchar()) != '\n' && c != EOF);
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF);
 }
 
 unsigned entreeUtilisateur(const char *msg, const char *msgErreur, unsigned min,
                            unsigned max) {
-   printf("%s [%u - %u] :", msg, min, max);
-   unsigned entree = 0;
-   while (scanf("%u", &entree) != 1 || entree < min || entree > max) {
-      viderBuffer();
-      printf("%s\n", msgErreur);
-      printf("%s [%u - %u] :", msg, min, max);
-   }
-   viderBuffer();
-   return entree;
-}
-
-
-unsigned separerGauche(unsigned valeur) {
-    unsigned valeurGauche = 0;
-    for(unsigned i = 0; i < valeur; ++i) {
-        if(rand() % 2) {
-            ++valeurGauche;
-        }
+    printf("%s [%u - %u] :", msg, min, max);
+    unsigned entree = 0;
+    while (scanf("%u", &entree) != 1 || entree < min || entree > max) {
+        viderBuffer();
+        printf("%s\n", msgErreur);
+        printf("%s [%u - %u] :", msg, min, max);
     }
-    return valeurGauche;
-}
-void imprimerCompteurs(unsigned* tableau, unsigned nbRangees)
-{
-   for (unsigned i = 1; i <= nbRangees; ++i) {
-      imprimerLigneCompteurs(tableau, i);
-   }
+    viderBuffer();
+    return entree;
 }
 
-void imprimerLigneCompteurs(unsigned* tableau, unsigned numLigne)
-{
-   for (unsigned i = 1; i <= numLigne; ++i) {
-      printf("%u ", tableau[(numLigne-1) + (i-1)]);
-   }
-   printf("\n");
+void imprimerCompteurs(unsigned *tableau, unsigned nbRangees) {
+    for (unsigned i = 1; i <= nbRangees; ++i) {
+        imprimerLigneCompteurs(tableau, i);
+    }
 }
+
+void imprimerLigneCompteurs(unsigned *tableau, unsigned numLigne) {
+    for (unsigned i = 1; i <= numLigne; ++i) {
+        printf("%u ", tableau[(numLigne - 1) + (i - 1)]);
+    }
+    printf("\n");
+}
+
+
+void parcoursBille(unsigned* tab, unsigned nbEtage) {
+    unsigned pos = 0;
+    for( unsigned etage = 1; etage <= nbEtage; ++etage) {
+        ++tab[pos];
+        if(rand() % 2) ++pos;
+
+        pos += etage;
+    }
+}
+
+
+unsigned nbClous(unsigned nbEtage) {
+    return nbEtage * (nbEtage + 1) / 2;
+}
+
+/* Fonction de check temp
+ *     for (unsigned etage = 0, index = 0; etage <= nbEtage; ++etage) {
+        unsigned summ = 0;
+        for (unsigned colonne = 0; colonne <= etage; ++colonne, ++index) {
+            summ+= tabCompteur[index];
+        }
+        printf("Check summ %d \n", summ);
+    }
+    printf("Check 1st value %d \n", tabCompteur[0]);
+    //nettoyage de la mémoire
+    */
